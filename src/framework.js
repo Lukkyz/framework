5fe5b11543d09ab83e24a7de4a8d0013da8c7d31
lucas <bienaimelucas@hotmail.fr>
2020-12-14 23:46:52 +0100

    Add tailwind css and add managing state between old and new node

--- src/framework.js
+++ src/framework.js
@@ -8,7 +8,6 @@ export class Component {
   }
   setState(newState) {
     this.state = Object.assign(this.state, newState);
-    console.log(this.state);
     if (this.oldNode) {
       this.updateComponent();
     }
@@ -17,6 +16,7 @@ export class Component {
     let old = this.oldNode;
     let parent = document.getElementById(this.id).parentNode;
     let newNode = this.render();
+    newNode.props.id = this.id;
     updateElement(parent, newNode, old);
     this.oldNode = newNode;
   }
@@ -30,8 +30,17 @@ export async function updateElement($parent, newNode, oldNode, index = 0) {
   if (!oldNode) {
     $parent.appendChild(await createElement(newNode));
   } else if (!newNode) {
+    if (typeof oldNode.type === "object") {
+      if (oldNode.type.onUnmount) {
+        oldNode.type.onUnmount();
+      }
+    }
     $parent.removeChild($parent.childNodes[index]);
   } else if (changed(newNode, oldNode)) {
+    if (newNode.props.id === oldNode.props.id) {
+      let oldDom = document.getElementById(`${oldNode.props.id}`);
+      oldDom = await createElement(newNode);
+    }
     if ($parent.childNodes[index]) {
       $parent.replaceChild(
         await createElement(newNode),
@@ -72,8 +81,10 @@ export async function createElement(node, component) {
   }
   if (typeof node.type === "function") {
     let obj = new node.type(node.props, node.children);
+    node.type = obj;
     if (obj.onMount) await obj.onMount();
     let html = obj.render();
+    html.props.id = obj.id;
     obj.oldNode = html;
     $el = await createElement(html, obj);
     if (node.props && node.props.className) {
@@ -108,12 +119,18 @@ export async function createElement(node, component) {
     if (node.props.onClick && node.component) {
       $el.addEventListener("click", node.props.onClick.bind(node.component));
     }
+
     if (node.props.id) {
       $el.id = node.props.id;
     }
     if (node.props.value) {
       $el.value = node.props.value;
-      console.log("d", $el);
+    }
+    if (node.props.type) {
+      $el.type = node.props.type;
+    }
+    if (node.props.checked) {
+      $el.checked = node.props.checked;
     }
   }
   return $el;
@@ -122,12 +139,30 @@ export async function createElement(node, component) {
 function changed(node1, node2) {
   return (
     typeof node1 !== typeof node2 ||
-    (typeof node1 === "string" && node1 != node2) ||
+    (typeof node1 === "string" && node1 !== node2) ||
     node1.type !== node2.type ||
     (typeof node1 === "object" &&
       typeof node2 === "object" &&
       node1.props &&
       node2.props &&
-      node1.props.title !== node2.props.title)
+      node1.props.title !== node2.props.title) ||
+    (typeof node1 === "object" &&
+      typeof node2 === "object" &&
+      node1.props &&
+      node2.props &&
+      node1.props.value !== node2.props.value) ||
+    (typeof node1 === "object" &&
+      typeof node2 === "object" &&
+      node1.props &&
+      node2.props &&
+      node1.props.className !== node2.props.className) ||
+    (typeof node1 === "object" &&
+      typeof node2 === "object" &&
+      node1.props &&
+      node2.props &&
+      node1.props.checked !== node2.props.checked) ||
+    (typeof node1 === "object" &&
+      typeof node2 === "object" &&
+      Object.keys(node1.props) !== Object.keys(node2.props))
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,58 @@
 import { h, Component, updateElement } from "./framework.js";
+import "tailwindcss/tailwind.css";
 import axios from "axios";
+
 /** @jsx h */
 
 // ---------------------------------------------------------------------
 class Post extends Component {
   constructor(props, children) {
     super(props, children);
+    this.state = {
+      completed: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    console.log(this);
+    this.props.completed = !this.props.completed;
+    this.updateComponent();
   }
   render() {
-    return <div>{this.props.title}</div>;
+    return (
+      <div
+        className={
+          this.props.completed ? "line-through bg-red-300" : "bg-green-300"
+        }
+      >
+        {this.props.title}
+        <input
+          type="checkbox"
+          onClick={this.onClick}
+          checked={this.props.completed}
+          name="state"
+        />
+        <label>{this.props.completed ? "Completed" : "Uncompleted"}</label>
+      </div>
+    );
   }
 }
 
 class Posts extends Component {
   constructor(props, children) {
     super(props, children);
-    this.state = { todos: "", newTodo: "" };
+    this.state = { date: "", todos: "", newTodo: "" };
   }
   async onMount() {
     let res = await axios("https://jsonplaceholder.typicode.com/todos/");
     let obj = res.data;
     this.setState({ todos: obj });
   }
+  onUnmount() {
+    clearInterval(this.timerID);
+  }
+
   onChange(e) {
     this.setState({
       newTodo: e.target.value,
@@ -36,34 +67,25 @@ class Posts extends Component {
   }
   render() {
     let todos = this.state.todos.map((child) => (
-      <Post
-        title={child.title}
-        className={child.completed ? "post line-through" : "post"}
-      />
+      <Post title={child.title} completed={child.completed} />
     ));
     return (
-      <div id={this.id} className="container">
-        <input
-          id="in"
-          value={this.state.newTodo}
-          onChange={this.onChange}
-        ></input>
+      <div id={this.id} className="bg-gray-100 container">
         <button className="btn" onClick={this.onClick}>
           Ajouter
         </button>
-        <p>{this.state.newTodo}</p>
+        <p>{this.state.date}</p>
         <div className="posts">{todos}</div>
       </div>
     );
   }
 }
 
-const a = (
-  <div>
-    <Posts />
-  </div>
-);
+const a = <Posts />;
 
 const $root = document.getElementById("root");
-
+const upload = document.querySelector("#upload");
+upload.addEventListener("click", () => {
+  updateElement($root, null, a);
+});
 updateElement($root, a);
